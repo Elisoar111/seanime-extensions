d7d74d80944941e73c8b2fa49241cd4a8d86c8f4
Elisoar <445168987@qq.com>
2026-07-22 21:55:29 +0800

    Update bangumi.plugin.js

--- Bangumi/bangumi.plugin.js
+++ Bangumi/bangumi.plugin.js
@@ -2,7 +2,7 @@
 /// <reference path="./plugin.d.ts" />
 function init() {
     $ui.register(function (ctx) {
-        console.log("[bangumi-ui] 插件已加载 v3.2.5");
+        console.log("[bangumi-ui] 插件已加载 v1.1.3");
         // =================================================================
         //  Constants
         // =================================================================
